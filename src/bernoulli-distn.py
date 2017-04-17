d931fcc029151e1f5e115955cd127418db30641c
Isaac Laughlin <isaac.laughlin@galvanize.com>
2017-04-17 16:08:04 -0700

    Fixing bernoulli plot.

--- src/bernoulli-distn.py
+++ src/bernoulli-distn.py
@@ -20,12 +20,13 @@ for p in [0.3,0.6,0.9]:
     splot += 1
     ax = fig.add_subplot(1,3,splot)
     
-    x = np.arange(scs.bernoulli.ppf(0.01, p),scs.bernoulli.ppf(0.99, p))
+    x = np.arange(scs.bernoulli.ppf(0.01, p),scs.bernoulli.ppf(0.99, p)+1)
     ax.plot(x, scs.bernoulli.pmf(x, p), 'bo', ms=8, label='pmf')
     ax.vlines(x, 0, scs.bernoulli.pmf(x, p), colors='b', lw=5, alpha=0.5)
     rv = scs.bernoulli(p)
     
     ax.set_ylim((0,1.0))
+    ax.set_xlim((-0.25, 1.25))
     ax.set_title("p=%s"%(p))
     ax.set_aspect(1./ax.get_data_ratio())
 
